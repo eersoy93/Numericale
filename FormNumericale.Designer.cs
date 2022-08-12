25d14cac3354bd7f74660bd6aca1a2cb0635bf30
Erdem Ersoy <erdemersoy256@gmail.com>
2022-08-12 15:23:08 +0300

    Add initial sources
    Signed-off-by: Erdem Ersoy <erdemersoy256@gmail.com>

---
+++ FormNumericale.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Numericale
+{
+    partial class FormNumericale
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonKey1 = new System.Windows.Forms.Button();
+            this.ButtonKey2 = new System.Windows.Forms.Button();
+            this.ButtonKey3 = new System.Windows.Forms.Button();
+            this.ButtonKey4 = new System.Windows.Forms.Button();
+            this.ButtonKey5 = new System.Windows.Forms.Button();
+            this.ButtonKey6 = new System.Windows.Forms.Button();
+            this.ButtonKey7 = new System.Windows.Forms.Button();
+            this.ButtonKey8 = new System.Windows.Forms.Button();
+            this.ButtonKey9 = new System.Windows.Forms.Button();
+            this.ButtonKeyAbout = new System.Windows.Forms.Button();
+            this.ButtonKey0 = new System.Windows.Forms.Button();
+            this.ButtonKeyEnter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ButtonKey1
+            // 
+            this.ButtonKey1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ButtonKey1.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ButtonKey1.Location = new System.Drawing.Point(12, 12);
+            this.ButtonKey1.Name = "ButtonKey1";
+            this.ButtonKey1.Size = new System.Drawing.Size(145, 114);
+            this.ButtonKey1.TabIndex = 0;
+            this.ButtonKey1.Text = "1";
+            this.ButtonKey1.UseVisualStyleBackColor = false;
+            this.ButtonKey1.Click += new System.EventHandler(this.ButtonKey1_Click);
+            // 
+            // ButtonKey2
+            // 
+            this.ButtonKey2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ButtonKey2.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ButtonKey2.Location = new System.Drawing.Point(163, 12);
+            this.ButtonKey2.Name = "ButtonKey2";
+            this.ButtonKey2.Size = new System.Drawing.Size(145, 114);
+            this.ButtonKey2.TabIndex = 1;
+            this.ButtonKey2.Text = "2";
+            this.ButtonKey2.UseVisualStyleBackColor = false;
+            this.ButtonKey2.Click += new System.EventHandler(this.ButtonKey2_Click);
+            // 
+            // ButtonKey3
+            // 
+            this.ButtonKey3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ButtonKey3.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ButtonKey3.Location = new System.Drawing.Point(314, 12);
+            this.ButtonKey3.Name = "ButtonKey3";
+            this.ButtonKey3.Size = new System.Drawing.Size(145, 114);
+            this.ButtonKey3.TabIndex = 2;
+            this.ButtonKey3.Text = "3";
+            this.ButtonKey3.UseVisualStyleBackColor = false;
+            this.ButtonKey3.Click += new System.EventHandler(this.ButtonKey3_Click);
+            // 
+            // ButtonKey4
+            // 
+            this.ButtonKey4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ButtonKey4.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ButtonKey4.Location = new System.Drawing.Point(12, 132);
+            this.ButtonKey4.Name = "ButtonKey4";
+            this.ButtonKey4.Size = new System.Drawing.Size(145, 114);
+            this.ButtonKey4.TabIndex = 3;
+            this.ButtonKey4.Text = "4";
+            this.ButtonKey4.UseVisualStyleBackColor = false;
+            this.ButtonKey4.Click += new System.EventHandler(this.ButtonKey4_Click);
+            // 
+            // ButtonKey5
+            // 
+            this.ButtonKey5.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ButtonKey5.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ButtonKey5.Location = new System.Drawing.Point(163, 132);
+            this.ButtonKey5.Name = "ButtonKey5";
+            this.ButtonKey5.Size = new System.Drawing.Size(145, 114);
+            this.ButtonKey5.TabIndex = 4;
+            this.ButtonKey5.Text = "5";
+            this.ButtonKey5.UseVisualStyleBackColor = false;
+            this.ButtonKey5.Click += new System.EventHandler(this.ButtonKey5_Click);
+            // 
+            // ButtonKey6
+            // 
+            this.ButtonKey6.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ButtonKey6.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ButtonKey6.Location = new System.Drawing.Point(314, 132);
+            this.ButtonKey6.Name = "ButtonKey6";
+            this.ButtonKey6.Size = new System.Drawing.Size(145, 114);
+            this.ButtonKey6.TabIndex = 5;
+            this.ButtonKey6.Text = "6";
+            this.ButtonKey6.UseVisualStyleBackColor = false;
+            this.ButtonKey6.Click += new System.EventHandler(this.ButtonKey6_Click);
+            // 
+            // ButtonKey7
+            // 
+            this.ButtonKey7.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ButtonKey7.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ButtonKey7.Location = new System.Drawing.Point(12, 252);
+            this.ButtonKey7.Name = "ButtonKey7";
+            this.ButtonKey7.Size = new System.Drawing.Size(145, 114);
+            this.ButtonKey7.TabIndex = 6;
+            this.ButtonKey7.Text = "7";
+            this.ButtonKey7.UseVisualStyleBackColor = false;
+            this.ButtonKey7.Click += new System.EventHandler(this.ButtonKey7_Click);
+            // 
+            // ButtonKey8
+            // 
+            this.ButtonKey8.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ButtonKey8.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ButtonKey8.Location = new System.Drawing.Point(163, 252);
+            this.ButtonKey8.Name = "ButtonKey8";
+            this.ButtonKey8.Size = new System.Drawing.Size(145, 114);
+            this.ButtonKey8.TabIndex = 7;
+            this.ButtonKey8.Text = "8";
+            this.ButtonKey8.UseVisualStyleBackColor = false;
+            this.ButtonKey8.Click += new System.EventHandler(this.ButtonKey8_Click);
+            // 
+            // ButtonKey9
+            // 
+            this.ButtonKey9.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ButtonKey9.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ButtonKey9.Location = new System.Drawing.Point(314, 252);
+            this.ButtonKey9.Name = "ButtonKey9";
+            this.ButtonKey9.Size = new System.Drawing.Size(145, 114);
+            this.ButtonKey9.TabIndex = 8;
+            this.ButtonKey9.Text = "9";
+            this.ButtonKey9.UseVisualStyleBackColor = false;
+            this.ButtonKey9.Click += new System.EventHandler(this.ButtonKey9_Click);
+            // 
+            // ButtonKeyAbout
+            // 
+            this.ButtonKeyAbout.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ButtonKeyAbout.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ButtonKeyAbout.Location = new System.Drawing.Point(12, 372);
+            this.ButtonKeyAbout.Name = "ButtonKeyAbout";
+            this.ButtonKeyAbout.Size = new System.Drawing.Size(145, 114);
+            this.ButtonKeyAbout.TabIndex = 9;
+            this.ButtonKeyAbout.Text = "ℹ";
+            this.ButtonKeyAbout.UseVisualStyleBackColor = false;
+            this.ButtonKeyAbout.Click += new System.EventHandler(this.ButtonKeyAbout_Click);
+            // 
+            // ButtonKey0
+            // 
+            this.ButtonKey0.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ButtonKey0.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ButtonKey0.Location = new System.Drawing.Point(163, 372);
+            this.ButtonKey0.Name = "ButtonKey0";
+            this.ButtonKey0.Size = new System.Drawing.Size(145, 114);
+            this.ButtonKey0.TabIndex = 10;
+            this.ButtonKey0.Text = "0";
+            this.ButtonKey0.UseVisualStyleBackColor = false;
+            this.ButtonKey0.Click += new System.EventHandler(this.ButtonKey0_Click);
+            // 
+            // ButtonKeyEnter
+            // 
+            this.ButtonKeyEnter.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ButtonKeyEnter.Font = new System.Drawing.Font("Segoe UI", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ButtonKeyEnter.Location = new System.Drawing.Point(314, 372);
+            this.ButtonKeyEnter.Name = "ButtonKeyEnter";
+            this.ButtonKeyEnter.Size = new System.Drawing.Size(145, 114);
+            this.ButtonKeyEnter.TabIndex = 11;
+            this.ButtonKeyEnter.Text = "↲";
+            this.ButtonKeyEnter.UseVisualStyleBackColor = false;
+            this.ButtonKeyEnter.Click += new System.EventHandler(this.ButtonKeyEnter_Click);
+            // 
+            // FormNumericale
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.ClientSize = new System.Drawing.Size(468, 498);
+            this.Controls.Add(this.ButtonKeyEnter);
+            this.Controls.Add(this.ButtonKey0);
+            this.Controls.Add(this.ButtonKeyAbout);
+            this.Controls.Add(this.ButtonKey9);
+            this.Controls.Add(this.ButtonKey8);
+            this.Controls.Add(this.ButtonKey7);
+            this.Controls.Add(this.ButtonKey6);
+            this.Controls.Add(this.ButtonKey5);
+            this.Controls.Add(this.ButtonKey4);
+            this.Controls.Add(this.ButtonKey3);
+            this.Controls.Add(this.ButtonKey2);
+            this.Controls.Add(this.ButtonKey1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormNumericale";
+            this.Text = "Numericale";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonKey1;
+        private System.Windows.Forms.Button ButtonKey2;
+        private System.Windows.Forms.Button ButtonKey3;
+        private System.Windows.Forms.Button ButtonKey4;
+        private System.Windows.Forms.Button ButtonKey5;
+        private System.Windows.Forms.Button ButtonKey6;
+        private System.Windows.Forms.Button ButtonKey7;
+        private System.Windows.Forms.Button ButtonKey8;
+        private System.Windows.Forms.Button ButtonKey9;
+        private System.Windows.Forms.Button ButtonKeyAbout;
+        private System.Windows.Forms.Button ButtonKey0;
+        private System.Windows.Forms.Button ButtonKeyEnter;
+    }
+}
+
---
+++ FormNumericale.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numericale
+{
+    public partial class FormNumericale : Form
+    {
+        public FormNumericale()
+        {
+            InitializeComponent();
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x08000000; // WS_DISABLED
+                return cp;
+            }
+        }
+
+        private void ButtonKeyAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Numericale 0.1\n" +
+                            "Copyright (c) 2022 Erdem Ersoy (eersoy93)\n" +
+                            "Licensed with GPLv3.\n" +
+                            "See LICENSE file for license text.",
+                            "About Numericale",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information);
+        }
+
+        private void ButtonKey1_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("1");
+        }
+
+        private void ButtonKey2_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("2");
+        }
+
+        private void ButtonKey3_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("3");
+        }
+
+        private void ButtonKey4_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("4");
+        }
+
+        private void ButtonKey5_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("5");
+        }
+
+        private void ButtonKey6_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("6");
+        }
+
+        private void ButtonKey7_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("7");
+        }
+
+        private void ButtonKey8_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("8");
+        }
+
+        private void ButtonKey9_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("9");
+        }
+
+        private void ButtonKey0_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("0");
+        }
+
+        private void ButtonKeyEnter_Click(object sender, EventArgs e)
+        {
+            SendKeys.Send("{ENTER}");
+        }
+    }
+}
